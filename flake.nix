aa2815a21f6a426b03a218f358754d84a74e6385
Dr.Teagle <chrisdockter@proton.me>
2024-10-13 13:59:45 -0500

    changed to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,36 +1,53 @@
 {
   description = "rustacean flake";
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rustacean.url = "github:mrcjkb/rustaceanvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { nixpkgs, rustacean, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell
-        {
-          buildInputs = with pkgs;[
-            llvm
-            rocmPackages.llvm.clang-tools-extra
-            libcxx
-            #rust
-            rustacean.packages.${system}.codelldb
-            cargo
-            rustc
-            rust-analyzer
-            rustfmt
-          ];
-          shellHook = ''
-            export SHELL=$(which zsh)
-            if [ -z "$ZSH_VERSION" ]; then
-              exec zsh
-            fi
-          '';
-        };
-    };
+  outputs = inputs@ { self, nixpkgs, flake-parts, rustacean, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        perSystem =
+          { config
+          , self'
+          , inputs'
+          , pkgs
+          , system
+          , ...
+          }:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            devShells.default = pkgs.mkShell
+              {
+                buildInputs = with pkgs;[
+                  libcxx
+                  gnumake
+                  cmake
+                  rocmPackages_6.llvm.clang
+                  clang-tools
+                  #rust
+                  rustacean.packages.${system}.codelldb
+                  cargo
+                  rustc
+                  rust-analyzer
+                  rustfmt
+                ];
+                shellHook = ''
+                  export SHELL=$(which zsh)
+                  if [ -z "$ZSH_VERSION" ]; then
+                    exec zsh
+                  fi
+                '';
+              };
+          };
 
+      };
 }
