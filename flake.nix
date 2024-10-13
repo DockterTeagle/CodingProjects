359c2d8f69798fde9c363aec870a5f5ee38c8a63
Dr.Teagle <chrisdockter@proton.me>
2024-10-13 13:22:05 -0500

    trimmed down flake, though now there is an issue with c lang not detecting the standard library

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rustacean.url = "github:mrcjkb/rustaceanvim";
   };
-  outputs = { self, nixpkgs, rustacean, ... }@inputs:
+  outputs = { nixpkgs, rustacean, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -14,12 +14,10 @@
       devShells.${system}.default = pkgs.mkShell
         {
           buildInputs = with pkgs;[
-            rocmPackages_6.llvm.clang-unwrapped
-            libgcc
-            glib
-            gcc-unwrapped
-            llvm_18
-            pkg-config
+            llvm
+            rocmPackages.llvm.clang-tools-extra
+            libcxx
+            #rust
             rustacean.packages.${system}.codelldb
             cargo
             rustc
@@ -32,58 +30,7 @@
               exec zsh
             fi
           '';
-          # shellHook = ''
-          #   rustup default stable
-          #   rustup component add clippy
-          #   export PATH="$HOME/.cargo/bin:$PATH"
-          #   export PATH="$PATH:${rustacean.packages.${system}.codelldb}/bin"
-          #   export PATH="${pkgs.rust-analyzer}/bin:$PATH"
-          #   export PATH="$PATH:${pkgs.rustfmt}/bin"
-          # '';
         };
     };
 
 }
-# {
-#   description = "rustacean flake";
-#   inputs = {
-#
-#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#     rustacean.url = "github:mrcjkb/rustaceanvim";
-#   };
-#   outputs = { self, nixpkgs, rustacean, ... }@inputs:
-#     let
-#       system = "x86_64-linux";
-#       pkgs = import nixpkgs { inherit system; };
-#     in
-#     {
-#       devShells.${system}.default = pkgs.mkShell
-#         {
-#           buildInputs = with pkgs;[
-#             clang
-#             libclang
-#             rocmPackages_6.llvm.clang-unwrapped
-#             libgcc
-#             glib
-#             gcc-unwrapped
-#             llvm_18
-#             pkg-config
-#             clang-tools
-#             rustacean.packages.${system}.codelldb
-#             rustc
-#             rustup
-#             rustfmt
-#             rust-analyzer
-#             cargo
-#           ];
-#           shellHook = ''
-#             rustup default stable
-#             rustup component add clippy
-#             export PATH="$HOME/.cargo/bin:$PATH"
-#             export PATH="$PATH:${rustacean.packages.${system}.codelldb}/bin"
-#             export PATH="${pkgs.rust-analyzer}/bin:$PATH"
-#             export PATH="$PATH:${pkgs.rustfmt}/bin"
-#           '';
-#         };
-#     };
-# }
