3e4cba2518e2b1d317962495394d96f4edab8b2f
Dr.Teagle <chrisdockter@proton.me>
2024-10-13 18:14:58 -0500

    added python projects flake

---
+++ pythonProjects/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Python Projects Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rustacean.url = "github:mrcjkb/rustaceanvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs@ { self, nixpkgs, flake-parts, rustacean, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        perSystem =
+          { config
+          , self'
+          , inputs'
+          , pkgs
+          , system
+          , ...
+          }:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            devShells.default = pkgs.mkShell
+              {
+                buildInputs = with pkgs;[
+                  zsh
+                  python312Full
+                  python312Packages.numpy_2
+                  python312Packages.scipy
+                  python312Packages.sympy
+                  python312Packages.seaborn
+                  python312Packages.matplotlib
+                  python312Packages.pandas
+                  python312Packages.pandas-stubs
+                  python312Packages.openpyxl
+                  # python312Packages.
+                ];
+                shellHook = ''
+                  export SHELL=$(which zsh)
+                  if [ -z "$ZSH_VERSION" ]; then
+                    exec zsh
+                  fi
+                '';
+              };
+          };
+      };
+}
