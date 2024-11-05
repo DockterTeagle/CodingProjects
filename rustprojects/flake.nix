db96e43353e0163d2b5baaceb8a018e741b0fa85
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-11-04 22:18:23 -0600

    removed file and also formatted flake

--- rustprojects/flake.nix
+++ rustprojects/flake.nix
@@ -5,37 +5,42 @@
     rustacean.url = "github:mrcjkb/rustaceanvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@ { self, nixpkgs, flake-parts, rustacean, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; }
-      {
-        systems = [
-          "x86_64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        perSystem =
-          { config
-          , self'
-          , inputs'
-          , pkgs
-          , system
-          , ...
-          }:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            devShells.default = pkgs.mkShell
-              {
-                buildInputs = with pkgs;[
-                  rustacean.packages.${system}.codelldb
-                  graphviz
-                  cargo
-                  rustc
-                  rust-analyzer
-                  rustfmt
-                ];
-              };
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      rustacean,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustacean.packages.${system}.codelldb
+              graphviz
+              cargo
+              rustc
+              rust-analyzer
+              rustfmt
+            ];
           };
-      };
+        };
+    };
 }
