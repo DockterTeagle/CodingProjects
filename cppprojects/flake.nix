ba56d07aae98d8a6c0b0ef36bacbfd6eb1c4ba58
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-11-25 10:19:37 -0600

    updated flake

--- cppprojects/flake.nix
+++ cppprojects/flake.nix
@@ -1,53 +1,70 @@
 {
-  description = "rustacean flake";
+  description = "cpp projects flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rustacean.url = "github:mrcjkb/rustaceanvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
-  outputs = inputs@ { self, nixpkgs, flake-parts, rustacean, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; }
-      {
-        systems = [
-          "x86_64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        perSystem =
-          { config
-          , self'
-          , inputs'
-          , pkgs
-          , system
-          , ...
-          }:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            devShells.default = pkgs.mkShell
-              {
-                buildInputs = with pkgs;[
-                  rustacean.packages.${system}.codelldb
-                  libcxx
-                  valgrind
-                  cmake
-                  rocmPackages.llvm.clang
-                  clang-tools
-                  codespell
-                  conan
-                  cppcheck
-                  doxygen
-                  gtest
-                  lcov
-                  vcpkg
-                  vcpkg-tool
-                ];
-                shellHook = ''
-                  export CODELLDB_PATH=${rustacean.packages.${system}.codelldb}
-                  echo $CODELLDB_PATH
-                '';
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      rustacean,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
+          checks = {
+            # pre-commit-check = inputs'.pre-commit-hooks.lib.run {
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixfmt-rfc-style.enable = true;
+                clang-format.enable = true;
+                clang-tidy.enable = true;
               };
+            };
           };
-      };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustacean.packages.${system}.codelldb
+              libcxx
+              valgrind
+              cmake
+              rocmPackages.llvm.clang
+              clang-tools
+              codespell
+              conan
+              cppcheck
+              doxygen
+              gtest
+              lcov
+              vcpkg
+              vcpkg-tool
+            ];
+            shellHook = # bash
+              ''
+                export CODELLDB_PATH=${rustacean.packages.${system}.codelldb}
+                echo $CODELLDB_PATH
+              '';
+          };
+        };
+    };
 }
