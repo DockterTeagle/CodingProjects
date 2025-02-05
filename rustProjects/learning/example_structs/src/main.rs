d0315d15742baeff0e11f6d934fa6970c9ff68b3
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-05 07:38:57 -0600

    chore: update flake inputs

--- rustProjects/learning/example_structs/src/main.rs
+++ rustProjects/learning/example_structs/src/main.rs
@@ -3,6 +3,20 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
 fn main() {
     let rect1 = Rectangle {
         width: 30,
